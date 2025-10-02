d89baa6280610fbbcbc697e1cec30582719faee1
YUNA <anyuna0@gmail.com>
2025-10-02 23:11:26 +0900

    AdvancedCalculator 주석추가
    https://github.com/yunaaaaaaaaaaaa/java_cal/issues/3

--- src/AdvancedCalculator.java
+++ src/AdvancedCalculator.java
@@ -11,7 +11,7 @@ public class AdvancedCalculator extends Calculator {
         return result;
     }
     // 계산기 v2.0 나누기 기능 개발 완료
-    //
+    //https://github.com/yunaaaaaaaaaaaa/java_cal/issues/3
     @Override
     public int divide()
     {
