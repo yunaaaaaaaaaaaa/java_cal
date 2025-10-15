60afb80926a4ebe38e6e9689c74acef894e6392c
YUNA <anyuna0@gmail.com>
2025-10-15 14:17:36 +0900

    .

---
+++ src/AdvancedCalculator.java
@@ -0,0 +1,21 @@
+// 계산기 v2.0 관련 클래스
+//https://github.com/yunaaaaaaaaaaaa/java_cal/issues/1
+
+public class AdvancedCalculator extends Calculator {
+    // 계산기 v2.0 곱하기 기능 개발 완료
+    //https://github.com/yunaaaaaaaaaaaa/java_cal/issues/2
+    @Override
+    public int multiply()
+    {
+        result = num1 * num2;
+        return result;
+    }
+    // 계산기 v2.0 나누기 기능 개발 완료
+    //https://github.com/yunaaaaaaaaaaaa/java_cal/issues/3
+    @Override
+    public int divide()
+    {
+        result = num1 / num2;
+        return result;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        Calculator calc_add = new Calculator();
 
-        calc_add.setNumbers(5, 3);
-        calc_add.add();
-        calc_add.displayResult("+");
-
-        Calculator calc_sub = new Calculator();
-        calc_sub.setNumbers(10, 3);
-        calc_sub.subtract();
-        calc_sub.displayResult("-");
-
-        Calculator calc_mul = new Calculator();
-        calc_mul.setNumbers(5, 3);
-        calc_mul.multiply();
-        calc_mul.displayResult("*");
-
-        Calculator calc_div = new Calculator();
-        calc_div.setNumbers(5, 3);
-        calc_div.divide();
-        calc_div.displayResult("/");
 
 
     }
