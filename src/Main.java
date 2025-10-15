574070e264f459696c0e9cd8dcf0c13673927c17
YUNA <anyuna0@gmail.com>
2025-10-15 14:24:52 +0900

    .

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        Calculator calc_add = new Calculator();
-        calc_add.setNumbers(5, 3);
-        calc_add.add();
-        calc_add.displayResult("+");
+     
 
-
-        AdvancedCalculator calc_mul = new AdvancedCalculator();
-        calc_mul.setNumbers(10, 2);
-        calc_mul.multiply();
-        calc_mul.displayResult("*");
-        
     }
 }
